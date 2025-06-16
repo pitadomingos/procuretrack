72b6706ae8e6017082db55e88768ac15de137572
Pita Domingos <pita.domingos@jachris.com>
2025-06-16 10:26:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/management/approvers/approver-form.tsx
+++ src/components/management/approvers/approver-form.tsx
@@ -21,6 +21,7 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
+  FormDescription, // Added FormDescription
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Switch } from '@/components/ui/switch';
