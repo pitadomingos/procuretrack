259af5b5bbf7130959f69390dfd7228a8f9d89dc
Pita Domingos <pita.domingos@jachris.com>
2025-06-18 05:24:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/survey/survey-form.tsx
+++ src/components/survey/survey-form.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import { useState } from 'react'; // Added missing import
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
